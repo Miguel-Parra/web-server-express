67793267de1902d099f5033cf1fa45d60d410077
Miguel-Parra <miguelts644@hotmail.com>
2022-03-16 14:48:23 -0500

    Despliegue app 2022-03-16

--- app.js
+++ app.js
@@ -5,33 +5,33 @@ const app = express()
 const puerto = process.env.PORT
 
 //Handlebars
-hbs.registerPartials(__dirname + '/views/partials', (err) => { console.log(err) });
-app.set('view engine', 'hbs'); //para que utilice hbs al renderizar usando app.render
+// hbs.registerPartials(__dirname + '/views/partials', (err) => { console.log(err) });
+// app.set('view engine', 'hbs'); //para que utilice hbs al renderizar usando app.render
 
 app.use(express.static('./public')); // Servir contenido estático 
 
-app.get('/', (req, res) => { //este es mi controlador en handlebars
-    res.render('home', {
-        nombre: 'Miguel',
-        titulo: 'Curso de node'
-    })
-})
+// app.get('/', (req, res) => { //este es mi controlador en handlebars
+//     res.render('home', {
+//         nombre: 'Miguel',
+//         titulo: 'Curso de node'
+//     })
+// })
 
-app.get('/elements', (req, res) => {
-    res.render('elements', {
-        nombre: 'Miguel',
-        titulo: 'Curso de node'
-    })
-})
-app.get('/generic', (req, res) => {
-    res.render('generic', {
-        nombre: 'Miguel',
-        titulo: 'Curso de node'
-    })
-})
+// app.get('/elements', (req, res) => {
+//     res.render('elements', {
+//         nombre: 'Miguel',
+//         titulo: 'Curso de node'
+//     })
+// })
+// app.get('/generic', (req, res) => {
+//     res.render('generic', {
+//         nombre: 'Miguel',
+//         titulo: 'Curso de node'
+//     })
+// })
 
 app.get('*', (req, res) => {
-    res.sendFile(`${__dirname}/public/404.html`)
+    res.sendFile(`${__dirname}/public/index.html`)
 })
 
 app.listen(puerto, () => {
